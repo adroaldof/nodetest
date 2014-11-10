60ce1b63a388cd0360fcf8101de7d61640f7e00c
Adroaldo de Andrade <adroaldof@gmail.com>
2014-11-10 14:32:19 -0200

    files

---
+++ cityModel.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+
+    CitySchema = new Schema({
+        _id: String,
+        cities: [{name: String, _id: String}]
+    });
+
+
+var City = mongoose.model('City', CitySchema);
+
+var saveToMongo = function(json, callback) {
+    var cities = new City(json);
+    cities.save(function (err) {
+        if (err) {
+            console.log('Mongo error: ' + err);
+        }
+
+        callback();
+    })
+}
+
+module.exports = {
+    'saveToMongo': saveToMongo
+};
---
+++ cityParser.js
@@ -0,0 +1,108 @@
+var fs = require('fs'),
+    _ = require('underscore'),
+    City = require('./cityModel'),
+    filePath = '',
+    citiesJSON = {},
+    mongoose = require('mongoose'),
+    reader = require ('buffered-reader'),
+    Join = require('join'),
+    states = {
+        'Acre': {'abbreviation': 'AC'},
+        'Alagoas': {'abbreviation': 'AL'},
+        'Amazonas': {'abbreviation': 'AM'},
+        'Amapá': {'abbreviation': 'AP'},
+        'Bahia': {'abbreviation': 'BA'},
+        'Ceará': {'abbreviation': 'CE'},
+        'Distrito Federal': {'abbreviation': 'DF'},
+        'Espírito Santo': {'abbreviation': 'ES'},
+        'Goiás': {'abbreviation': 'GO'},
+        'Maranhão': {'abbreviation': 'MA'},
+        'Minas Gerais': {'abbreviation': 'MG'},
+        'Mato Grosso do Sul': {'abbreviation': 'MS'},
+        'Mato Grosso': {'abbreviation': 'MT'},
+        'Pará': {'abbreviation': 'PA'},
+        'Paraíba': {'abbreviation': 'PB'},
+        'Pernambuco': {'abbreviation': 'PE'},
+        'Piauí': {'abbreviation': 'PI'},
+        'Paraná': {'abbreviation': 'PR'},
+        'Rio de Janeiro': {'abbreviation': 'RJ'},
+        'Rio Grande do Norte': {'abbreviation': 'RN'},
+        'Rondônia': {'abbreviation': 'RO'},
+        'Roraima': {'abbreviation': 'RR'},
+        'Rio Grande do Sul': {'abbreviation': 'RS'},
+        'Santa Catarina': {'abbreviation': 'SC'},
+        'Sergipe': {'abbreviation': 'SE'},
+        'São Paulo': {'abbreviation': 'SP'},
+        'Tocantins': {'abbreviation': 'TO'}
+    };
+
+filePath = process.argv[2];
+
+var pushToMongo = function (data) {
+    var join = Join.create();
+
+    mongoose.connect('mongodb://localhost/City');
+
+    _.each(Object.keys(data), function (state) {
+        City.saveToMongo({_id:state, cities:data[state]}, join.add());
+    });
+
+    join.when(function () {
+        mongoose.disconnect();
+    });
+};
+
+var generateJson = function (data) {
+    var line = data.split('\t'),
+        state = line[1],
+        stateAbbrev = states[state].abbreviation;
+
+    citiesJSON[stateAbbrev] = citiesJSON[stateAbbrev] ? citiesJSON[stateAbbrev] : [];
+
+    var city = {
+        '_id': String(line[0]) + String(line[2]),
+        'name': String(line[3].split('\r')[0])
+    };
+
+    if (_.isNumber(parseInt(line[0]))) {
+        if (!_.findWhere(citiesJSON[stateAbbrev], city)) {
+            citiesJSON[stateAbbrev].push(city);
+        }
+    }
+};
+
+var readLines = function (input, callback) {
+    var remaining = '';
+
+    input.on('data', function (data) {
+        remaining += data;
+        var index =  remaining.indexOf('\n'),
+            last = 0;
+
+        while (index > -1) {
+            var line = remaining.substring(0, index);
+            remaining = remaining.substring(index + 1);
+            if (line.toLowerCase().indexOf('uf') < 0) {
+                callback(line);
+            }
+            index =  remaining.indexOf('\n', last);
+        }
+
+        remaining = remaining.substring(last);
+    });
+
+    input.on('error', function (err) {
+        console.log('Error: ' + err);
+    })
+
+
+    input.on('end', function () {
+        if (remaining.length > 0) {
+            callback(remaining);
+        }
+        pushToMongo(citiesJSON);
+    });
+};
+
+var input = fs.createReadStream(filePath, {encoding: 'utf16le'});
+readLines(input, generateJson);
